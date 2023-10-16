db952cee803efdf0f0ecbd14ce2c857fadb5f7f6
mr7282 <mr7282@mail.ru>
2023-10-16 19:27:47 +0300

    Failed test fixed

--- internal/config/flag.go
+++ internal/config/flag.go
@@ -8,6 +8,6 @@ var BaseAddr *string
 
 func ParseFlag() {
 	ServAddr = flag.String("a", "localhost:8080", "server start address")
-	BaseAddr = flag.String("b", *ServAddr, "base response address")
+	BaseAddr = flag.String("b", "http://" + *ServAddr, "base response address")
 	flag.Parse()
 }
